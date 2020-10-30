6040c06c8bcdcd8033a7746c2b3994df42c29648
Rickey Dong <rickeyd2004@gmail.com>
2020-10-30 15:27:47 -0400

    accounted for situation where a point might be null, leading to NullPointerException

--- Point.java
+++ Point.java
@@ -25,6 +25,6 @@ public class Point
   }
   public boolean equals(Point other)
   {
-    return this.x == other.x && this.y == other.y;
+    return other != null && this.x == other.x && this.y == other.y;
   }
 }
